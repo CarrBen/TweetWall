1dee5f428767f4a6004b75a2b98c64c649f94510
Ben Carr <ben.carr2@ntlworld.com>
2015-11-08 21:07:56 +0000

    Switching in place with a basic demo

--- js/tweetwall.js
+++ js/tweetwall.js
@@ -1,10 +1,22 @@
 function initGrid(width, height){
 	var grid = $("#grid");
+	//Abort if there is no grid container
+	if(grid.length < 1){
+		return false;
+	}
 	grid.empty();
+	
+	//Check we have a gridStyle and create one if we don't
+	var gridStyle = $("#gridStyle");
+	if(gridStyle.length < 1){
+		gridStyle = $("<style id='gridStyle'></style>");
+		$("head").append(gridStyle);
+	}
+	//Set row and cell width and height to match requested width and height
 	setGridCss(width, height);
 	
 	for(var y=0; y<height; y+=1){
-		makeRow(grid, width);
+		makeRow(grid, width, y);
 	}
 }
 
@@ -16,17 +28,19 @@ function setGridCss(width, height){
 				".cell{height:100%; width:" + cellWidth + ";}");
 }
 
-function makeRow(grid, width){
+function makeRow(grid, width, y){
 	var currentRow = $("<div></div>");
 	currentRow.addClass("row");
 	grid.append(currentRow);
 	
 	for(var x=0; x<width; x+=1){
-		makeCell(currentRow);
+		makeCell(currentRow, x, y);
 	}
+	
+	currentRow.attr("id", "row_" + y);
 }
 
-function makeCell(row){
+function makeCell(row, x, y){
 	var cell = $("<div></div>");
 	row.append(cell);
 	cell.addClass("cell")
@@ -36,6 +50,8 @@ function makeCell(row){
 	
 	var switcherBack = makeSwitcher("")
 	cell.append(switcherBack);
+	
+	cell.attr("id", "cell_" + x + "-" + y);
 }
 
 function makeSwitcher(classes){
@@ -65,4 +81,15 @@ function makeTweet(switcher){
 	switcher.append(wrapper);
 	wrapper.append(container);
 	container.append(tweet);
+}
+
+function switchCell(x, y, direction){
+	var cell = $("#cell_" + x + "-" + y);
+	var current = $(".switcher.current", cell);
+	var background = $(".switcher:not(.current)", cell)
+	current.addClass(direction);
+	setTimeout(function(){
+		current.removeClass(direction).removeClass("current");
+		background.addClass("current");				
+	}, 1000);
 }
